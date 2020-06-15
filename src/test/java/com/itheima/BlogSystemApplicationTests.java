0f7549ecb440d15209c3a3bf7bbe03cb162eb894
mafeifan <1184045779@qq.com>
2020-06-15 19:51:23 +0800

    jee实训

---
+++ src/test/java/com/itheima/BlogSystemApplicationTests.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
